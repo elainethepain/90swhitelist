07ab7897e219845e3153c8ccd6f50782c43c864d
elainethepain <80547818+elainethepain@users.noreply.github.com>
2023-04-13 15:53:52 -0700

    adding chakra

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FuelProvider, createClient } from 'fuels-react';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App';
 
 const client = createClient({ chains: ['beta-2'] });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FuelProvider client={client}>
-      <App />
-    </FuelProvider>
+    <ChakraProvider>
+      <FuelProvider client={client}>
+        <App />
+      </FuelProvider>
+    </ChakraProvider>
   </React.StrictMode>,
 );
 
